0c4a0604dfaaa981f4f2aa7d25817356a3aa92bc
PRADDUMAN <32358024+Pradduman@users.noreply.github.com>
2017-12-21 14:14:43 +0530

    Add files via upload

---
+++ pradduman.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+int a[3][3],i,j,s=0,d,c=1;
+printf("Enter a matrix");
+for(i=0;i<3;i++)
+{
+for(j=0;j<3;j++)
+scanf("%d",&a[i][j]);
+}
+for(i=0;i<1;i++)
+
+{
+s=s+c*
+}
+printf("%d\n",s);
+}
+
+for(i=0;i<3;i++)
+{
+d=0;
+for(j=0;j<3;j++)
+{
+d=d+a[i][j];
+}
+printf("%d\n",d);
+}}
+
